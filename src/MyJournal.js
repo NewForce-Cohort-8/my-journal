63e993b0c35600ace6c39b0646e9357071b6dd39
Tommy-Spurlock <tspurloc@gmail.com>
2023-10-23 13:32:40 -0400

    first commit. added bulma framework

---
+++ src/MyJournal.js
@@ -0,0 +1,30 @@
+
+
+export const MyJournal = () => {
+
+
+    return (
+        
+    <section className="section">
+        <div className="container">
+            <h1 className="title">
+                Hello World
+            </h1>
+            <p className="subtitle">
+                 My first website with <strong>Bulma</strong>!
+            </p>
+    
+    <div className="field">
+        <label className="label">Label</label>
+        <div className="control">
+            <input className="input" type="text" placeholder="Text input" />
+        </div>
+            <p className="help">This is a help text</p>
+        </div>
+    </div>
+    </section>
+    
+    )
+
+
+}
