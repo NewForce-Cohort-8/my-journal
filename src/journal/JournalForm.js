0f5cba3018ff6b71e8b0b7274991e3396463681b
Tommy-Spurlock <tspurloc@gmail.com>
2023-10-26 12:05:31 -0400

    Form now works! added options to mood select. Still need to repopulate journalentries list

--- src/journal/JournalForm.js
+++ src/journal/JournalForm.js
@@ -1,35 +1,62 @@
+import { useState } from "react"
 import { MoodSelect } from "./MoodSelect"
 
 
 
 export const JournalForm = () => {
+ const [journalEntry, setJournalEntry] = useState({})
 
+ const handleControlledInputChange = (e) => {
 
+    const newJournalEntry = {...journalEntry}
+
+    newJournalEntry[`${e.target.name}`] = e.target.value
+
+    setJournalEntry(newJournalEntry)
+ }
+    
+
+ const saveEntry = (e) => {
+    e.preventDefault()
+
+    const entryToSend = {...journalEntry}
+    entryToSend.moodId = +entryToSend.moodId
+    entryToSend.userId = 1
+
+    
+        fetch("http://localhost:8088/journalEntries", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(entryToSend),
+        })
+ }
 
 
     return ( 
-        <form>
+        <form onSubmit={saveEntry}>
             <div className="field">
                 <label className="label">Title</label>
                 <div className="control">
-                    <input className="input" type="text" placeholder="Name your entry" />
+                    <input name="title" className="input" type="text" placeholder="Name your entry" value={journalEntry.title} onChange={handleControlledInputChange}/>
                 </div>
             </div>
             <div className="field">
                 <label className="label">Date</label>
                 <div className="control">
-                    <input className="input" type="Date"  />
+                    <input className="input" type="Date" name="dateTime" value={journalEntry.dateTime}  onChange={handleControlledInputChange}/>
                 </div>
             </div>
-            <MoodSelect />
+            <MoodSelect handleControlledInputChange={handleControlledInputChange} journalEntry={journalEntry} />
             <div className="field">
                 <label className="label">Write it</label>
                 <div className="control">
-                    <textarea className="textarea" placeholder="tell me about it ..."></textarea> 
+                    <textarea name="entryText" className="textarea" placeholder="tell me about it ..." value={journalEntry.entryText}  onChange={handleControlledInputChange}></textarea> 
                 </div>
             </div>
             <div className="control">
-                <button className="button is-primary">Submit</button>
+                <button type="submit" className="button is-primary">Submit</button>
             </div>
      
         </form>
--- src/journal/MoodSelect.js
+++ src/journal/MoodSelect.js
@@ -1,10 +1,16 @@
+import { useEffect, useState } from "react"
 
 
 
-export const MoodSelect = () => {
+export const MoodSelect = ({handleControlledInputChange, journalEntry}) => {
+    const [moods, setMoods] = useState([])
 
 
-
+    useEffect(() =>{
+        fetch("http://localhost:8088/moods")
+        .then(r => r.json())
+        .then(moods => setMoods(moods))
+    },[])
 
     return ( 
 
@@ -12,10 +18,11 @@ export const MoodSelect = () => {
             <label className="label">Mood</label>
             <div className="control">
                 <div className="select">
-                    <select>
+                    <select name="moodId" onChange={handleControlledInputChange} value={journalEntry.moodId}>
                         <option>Select dropdown</option>
-                        <option>With options</option>
-                        {/* //map option tags here  */}
+                        {
+                            moods.map(mood => <option key={mood.id} value={mood.id}>{mood.name}</option>)
+                        }
                     </select>
                 </div>
              </div>
